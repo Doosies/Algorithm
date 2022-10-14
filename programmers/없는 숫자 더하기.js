33fbf1dfaa38bea0bb2109ea4423a63ce86325fc
Doosies <song961003@gmail.com>
2022-10-14 18:45:10 +0900

    프로그래머스: 없는숫자 더하기

---
+++ programmers/없는 숫자 더하기.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+  let result = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+  return result - numbers.reduce((sum, now) => sum + now, 0);
+}
+
+console.log(solution([1, 2, 3, 4, 6, 7, 8, 0]));
