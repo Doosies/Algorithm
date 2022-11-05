283002d0ac89f78b50aecce387afad764c6eba4b
Doosies <song961003@gmail.com>
2022-11-05 16:01:29 +0900

    boj#1105: 팔.js

---
+++ boj/#1105: 팔.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync(0, 'utf-8') : `128 138`).trim().split('\n');
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++].split(' ').map(v => v);
+})();
+
+const [L, R] = input();
+let cnt = 0;
+
+if (L.length === R.length) {
+  for (let i = 0; i < L.length; i++) {
+    if (L[i] === R[i] && L[i] === '8') {
+      cnt++;
+    } else if (L[i] !== R[i]) break;
+  }
+}
+console.log(cnt);
