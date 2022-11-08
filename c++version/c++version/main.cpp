90408b73f554e7054276e0ab3b79251675f0160a
Doosies <song961003@gmail.com>
2022-11-08 18:56:32 +0900

    c++version for xcode

---
+++ c++version/c++version/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++version
+//
+//  Created by 송민형 on 2022/11/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
