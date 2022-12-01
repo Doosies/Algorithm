111e660a61dfbc82032ed984efad7fea300ece45
Doosies <song961003@gmail.com>
2022-12-01 10:11:25 +0900

    프로그래머스: 상각달팽이

---
+++ JavascriptVersion/programmers/삼각 달팽이.js
@@ -0,0 +1,26 @@
+const solution = n => {
+  const answer = Array.from({ length: n }, (_, i) => Array(1).fill(i + 1));
+  const pos = [
+    [0, 1],
+    [-1, -1],
+    [1, 0],
+  ];
+  let x = 0;
+  let y = n - 1;
+  let now = n;
+  let move = n - 1;
+
+  for (let i = 0; i < n; i++) {
+    const [ny, nx] = pos[i % 3];
+    for (let j = 0; j < move; j++) {
+      y += ny;
+      x += nx;
+      now++;
+      answer[y][x] = now;
+    }
+    move--;
+  }
+  return answer.reduce((arr, now) => arr.concat(now), []);
+};
+
+console.log(solution(5));
