721431191601b8a5b87585bab4571021060fe6e0
Doosies <song961003@gmail.com>
2023-05-06 22:36:41 +0900

    #14852: 타일 채우기 3

--- JavascriptVersion/boj/#14852: 타일 채우기 3.js
+++ JavascriptVersion/boj/#14852: 타일 채우기 3.js
@@ -3,14 +3,16 @@
 const stdin = `5`.trim().split('\n');
 const N = +stdin[0];
 const dp = Array(1_000_001).fill(0);
-const memo = [0, 4, 18];
+const memo = [2, 6, 20]; // 원래는 [0, 4, 18] 이지만 유니크 패턴 2를 더해서 왼쪽과 같음
 dp[1] = 2;
 dp[2] = 7;
 
 for (let i = 3; i <= N; i++) {
-  const calc = dp[i - 1] * 2 + dp[i - 2] * 3 + memo[i - 3] + 2;
+  const calc = dp[i - 1] * 2 + dp[i - 2] * 3 + memo[i - 3];
+  const memoCalc = memo[i - 1] + dp[i] * 2;
+
   dp[i] = calc % 1_000_000_007;
-  memo[i] = (memo[i - 1] + dp[i] * 2) % 1_000_000_007;
+  memo[i] = memoCalc % 1_000_000_007;
 }
 
 console.log(dp[N]);
