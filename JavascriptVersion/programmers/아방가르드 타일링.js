ceb32e1fb92d453c80b79e2e43381181322dcf78
Doosies <song961003@gmail.com>
2023-05-07 21:43:54 +0900

    아방가르드 타일링 타른풀이

--- JavascriptVersion/programmers/아방가르드 타일링.js
+++ JavascriptVersion/programmers/아방가르드 타일링.js
@@ -1,26 +1,33 @@
+// function solution(n) {
+//   const dp = [1, 1, 3, 10, 23, 62, 170, 441, 1173];
+//   const memo = [
+//     [2, 6, 12, 32], // 2 4 2 2 4 2 2 4 2 2 4  0
+//     [2, 4, 16, 36], // 2 2 4 2 2 4 2  1
+//     [4, 6, 12, 52], // 4 2 2 4 2 2 4  2
+//   ];
+
+//   for (let i = 4; i <= n; i++) {
+//     const now = (i - 1) % 3;
+//     const t1 = (now + 1) % 3;
+//     const t2 = (now + 2) % 3;
+
+//     dp[i] = (dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + memo[now][i - 4]) % 1000000007;
+
+//     memo[now][i] = (memo[now][i - 1] + dp[i] * 4) % 1000000007;
+//     memo[t1][i] = (memo[t1][i - 1] + dp[i] * 2) % 1000000007;
+//     memo[t2][i] = (memo[t2][i - 1] + dp[i] * 2) % 1000000007;
+//   }
+
+//   return dp[n] % 1000000007;
+// }
 function solution(n) {
-  const dp = [1, 1, 3, 10, 23, 62, 170, 441, 1173];
-  const memo = [
-    [2, 6, 12, 32], // 2 4 2 2 4 2 2 4 2 2 4  0
-    [2, 4, 16, 36], // 2 2 4 2 2 4 2  1
-    [4, 6, 12, 52], // 4 2 2 4 2 2 4  2
-  ];
-
-  for (let i = 4; i <= n; i++) {
-    const now = (i - 1) % 3;
-    const t1 = (now + 1) % 3;
-    const t2 = (now + 2) % 3;
-
-    dp[i] = (dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5 + memo[now][i - 4]) % 1000000007;
-
-    memo[now][i] = (memo[now][i - 1] + dp[i] * 4) % 1000000007;
-    memo[t1][i] = (memo[t1][i - 1] + dp[i] * 2) % 1000000007;
-    memo[t2][i] = (memo[t2][i - 1] + dp[i] * 2) % 1000000007;
+  const mod = 1000000007;
+  const dp = [1, 1, 3, 10, 23, 62];
+  for (let i = 6; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 6 + dp[i - 4] - dp[i - 6]) % mod;
   }
-
-  return dp[n] % 1000000007;
+  console.log(dp);
+  return dp[n];
 }
-const arr = [1, 1, 3, 10, 23, 62, 170, 441, 1173, 3127];
 
-const ans = solution(10);
-console.log(ans);
+const a = solution(100000);
