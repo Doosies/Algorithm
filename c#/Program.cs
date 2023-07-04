f3d1c20ff45268b36bb61c040c9c20791c68a38a
Song_Minhyung <song961003@gmail.com>
2023-07-04 12:05:02 +0900

    C# test

---
+++ c#/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program {
+  static void Main(string[] args) {
+    Child c = new Child();
+    c.Print();
+  }
+}
+interface Ifunc
+{
+  void Print();  
+}
+
+class Parent : Ifunc
+{
+    public virtual void Print() {
+        Console.WriteLine("Parent");
+    }
+}
+
+class Child : Parent
+{
+    
+    // public override void Print() {
+    //     Console.WriteLine("Child");
+    // }
+}
+
