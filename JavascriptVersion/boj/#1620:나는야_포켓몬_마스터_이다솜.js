ad4fcb0f54888aa0b4d5ecc3965d1abe9b3af32f
Song_Minhyung <song961003@gmail.com>
2023-08-26 18:26:05 +0900

    #1620: 나는야 포켓몬 마스터 이다솜

---
+++ JavascriptVersion/boj/#1620:나는야_포켓몬_마스터_이다솜.js
@@ -0,0 +1,59 @@
+// 문제링크: https://www.acmicpc.net/problem/1620
+// 시작날짜: 2023.08.26
+// 시작시간: 18:17
+// 종료시간:
+// 소요시간:
+
+//const stdin = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+//prettier-ignore
+const stdin = ` 
+26 5
+Bulbasaur
+Ivysaur
+Venusaur
+Charmander
+Charmeleon
+Charizard
+Squirtle
+Wartortle
+Blastoise
+Caterpie
+Metapod
+Butterfree
+Weedle
+Kakuna
+Beedrill
+Pidgey
+Pidgeotto
+Pidgeot
+Rattata
+Raticate
+Spearow
+Fearow
+Ekans
+Arbok
+Pikachu
+Raichu
+25
+Raichu
+3
+Pidgey
+Kakuna
+`.trim().split('\n');
+//prettier-ignore
+const input = (() => { let l = 0; return () => stdin[l++].split(' ')})();
+
+const [N, M] = input().map(Number);
+const map = new Map();
+let result = '';
+
+for (let i = 1; i < N + 1; i++) {
+  const name = input().toString();
+  map.set(i.toString(), name);
+  map.set(name, i);
+}
+for (let i = 0; i < M; i++) {
+  const quest = input().toString();
+  result += `${map.get(quest)}\n`;
+}
+console.log(result);
